48f556335b38c6dcb5a46b947497d1bef2210ec6
Antidote <antidote@wiiking2.com>
2013-09-29 12:35:51 -0700

    * [SpriteEditor] Fixed compiling

---
+++ SpriteEditorPlugin/include/SpriteEditorPlugin.hpp
@@ -0,0 +1,71 @@
+// This file is part of WiiKing2 Editor.
+//
+// WiiKing2 Editor is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Wiiking2 Editor is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with WiiKing2 Editor.  If not, see <http://www.gnu.org/licenses/>
+
+#ifndef SPRITEEDITORPLUGIN_HPP
+#define SPRITEEDITORPLUGIN_HPP
+
+#include <QObject>
+#include "spriteeditorplugin_global.hpp"
+#include <QIcon>
+#include "PluginInterface.hpp"
+
+class SPRITEEDITORPLUGIN_EXPORT SpriteEditorPlugin : public QObject, public PluginInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(PluginInterface)
+#if QT_VERSION >= 0x050000
+    Q_PLUGIN_METADATA(IID "org.wiiking2.spriteeditorplugin" FILE "spriteeditor.json")
+#endif
+public:
+    SpriteEditorPlugin();
+    ~SpriteEditorPlugin();
+
+    void initialize(MainWindowBase *mainWindow);
+    QString filter()      const;
+    QString extension()   const;
+    QString name()        const;
+    QString author()      const;
+    QString version()     const;
+    QString website()     const;
+    void setPath(const QString &path);
+    QString path()        const;
+    QString license()     const;
+    QString description() const;
+    bool enabled() const;
+    void setEnabled(const bool enable);
+
+    DocumentBase* loadFile(const QString& file) const;
+    bool canLoad(const QString& filename);
+
+    QDialog* settingsDialog();
+    Updater* updater();
+    QObject* object();
+
+    QIcon icon() const;
+    bool hasUpdater() const;
+    void doUpdate();
+    MainWindowBase* mainWindow() const;
+signals:
+    void enabledChanged();
+public slots:
+
+private:
+    bool m_enabled;
+    QString m_path;
+    QIcon m_icon;
+    MainWindowBase* m_mainWindow;
+};
+
+#endif // SPRITEEDITORPLUGIN_HPP
---
+++ SpriteEditorPlugin/src/SpriteEditorPlugin.cpp
@@ -0,0 +1,152 @@
+// This file is part of WiiKing2 Editor.
+//
+// WiiKing2 Editor is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Wiiking2 Editor is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with WiiKing2 Editor.  If not, see <http://www.gnu.org/licenses/>
+
+#include "SpriteEditorPlugin.hpp"
+#include "DocumentBase.hpp"
+
+
+SpriteEditorPlugin::SpriteEditorPlugin()
+    : m_enabled(true)
+{
+}
+
+SpriteEditorPlugin::~SpriteEditorPlugin()
+{
+
+}
+
+void SpriteEditorPlugin::initialize(MainWindowBase *mainWindow)
+{
+    m_mainWindow = mainWindow;
+}
+
+QString SpriteEditorPlugin::filter() const
+{
+    return "Sakura Engine Sprites *.sprs (*.sprs)";
+}
+
+QString SpriteEditorPlugin::extension() const
+{
+    return "sprs";
+}
+
+QString SpriteEditorPlugin::name() const
+{
+    return "Sprite Editor Plugin";
+}
+
+QString SpriteEditorPlugin::author() const
+{
+    return "Phillip \"Antidote\" Stephens";
+}
+
+QString SpriteEditorPlugin::version() const
+{
+    return "0.0.1";
+}
+
+QString SpriteEditorPlugin::website() const
+{
+    return "http://wiiking2.com";
+}
+
+void SpriteEditorPlugin::setPath(const QString& path)
+{
+    if (!m_path.isEmpty())
+        return;
+
+    m_path = path;
+}
+
+QString SpriteEditorPlugin::path() const
+{
+    if (m_path.isEmpty())
+        return "Unknown";
+    return m_path;
+}
+
+QString SpriteEditorPlugin::license() const
+{
+    return "GPLv3";
+}
+
+QString SpriteEditorPlugin::description() const
+{
+    return "Plugin for editing and creating Sprites for the Sakura game engine.";
+}
+
+bool SpriteEditorPlugin::enabled() const
+{
+    return m_enabled;
+}
+
+void SpriteEditorPlugin::setEnabled(const bool enable)
+{
+    if (m_enabled == enable)
+        return;
+
+    m_enabled = enable;
+
+    emit enabledChanged();
+}
+
+DocumentBase* SpriteEditorPlugin::loadFile(const QString& file) const
+{
+    return new DocumentBase(this, file);
+}
+
+bool SpriteEditorPlugin::canLoad(const QString& filename)
+{
+    return (!QString::compare(QFileInfo(filename).suffix(), extension(), Qt::CaseInsensitive));
+}
+
+bool SpriteEditorPlugin::hasUpdater() const
+{
+    return false;
+}
+
+void SpriteEditorPlugin::doUpdate()
+{
+
+}
+
+MainWindowBase*SpriteEditorPlugin::mainWindow() const
+{
+    return m_mainWindow;
+}
+
+Updater* SpriteEditorPlugin::updater()
+{
+    return NULL;
+}
+
+QDialog* SpriteEditorPlugin::settingsDialog()
+{
+    return NULL;
+}
+
+QObject* SpriteEditorPlugin::object()
+{
+    return this;
+}
+
+QIcon SpriteEditorPlugin::icon() const
+{
+    return m_icon;
+}
+
+#if QT_VERSION < 0x050000
+Q_EXPORT_PLUGIN(SpriteEditorPlugin)
+#endif
