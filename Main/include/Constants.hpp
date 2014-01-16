3fdd9bfe771da08ed8dd8978e84b44ebe41b0bcb
Antidote <antidote.crk@gmail.com>
2014-01-15 18:45:18 -0800

    * Release 1.0.0

--- Main/include/Constants.hpp
+++ Main/include/Constants.hpp
@@ -29,9 +29,9 @@
 namespace Constants
 {
 #define tr QObject::tr
-const int     SAKURASUITE_MAJOR                    = 0;
-const int     SAKURASUITE_MINOR                    = 1;
-const int     SAKURASUITE_PATCH                    = 2;
+const int     SAKURASUITE_MAJOR                    = 1;
+const int     SAKURASUITE_MINOR                    = 0;
+const int     SAKURASUITE_PATCH                    = 0;
 const int     SAKURASUITE_VERSION                  = (SAKURASUITE_MAJOR << 16) | (SAKURASUITE_MINOR << 8) | SAKURASUITE_PATCH;
 const QString SAKURASUITE_VERSION_STR              = QString("%1.%2.%3")
                                                   .arg(SAKURASUITE_MAJOR)
